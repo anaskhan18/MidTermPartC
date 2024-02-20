7ba3f26a19f8c43fca04686a17d7f1bfaa5b8546
khani <khani@10.16.53.232>
2024-02-20 12:31:43 -0500

    Added comments in anas.java class

---
+++ src/anas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * 
+ * @author khani
+ */
+//I there, I am adding some comments here!
+public class anas {
+    
+}
